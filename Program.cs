1dd69ed7bf2c47773e4a7937ecd04f38a8fb304a
MatheusAlmeidaNascimento <86276081+MatheusAlmeidaNascimento@users.noreply.github.com>
2021-06-25 23:09:13 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, multiplicador, tabuada;
+            numero = 0;
+            multiplicador = 1;
+            Console.WriteLine("Tabuada iniciada");
+
+            while (numero < 10)
+            {
+                numero++;
+                Console.WriteLine($"\nTabuada do {numero}");
+                multiplicador = 1;
+                while (multiplicador <= 10)
+                {
+                    tabuada = numero * multiplicador;
+                    Console.WriteLine($"{numero} x {multiplicador} = {tabuada}");
+                    multiplicador++;
+                }
+            }
+        }
+    }
+}
